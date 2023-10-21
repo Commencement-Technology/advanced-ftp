1a23be7f5c5550097ef9a5a67faf045691214dcc
RythenGlyth <rythenglyth@gmail.com>
2023-10-21 19:22:30 +0200

    lint fixes

--- src/FTPMaster.ts
+++ src/FTPMaster.ts
@@ -1,98 +1,98 @@
-import { AccessOptions, Client } from "./Client";
+import { AccessOptions, Client } from "./Client"
 
 interface QueuedFTPTask<T = any> {
-    promise: (client: Client) => Promise<T>;
-    resolve: (value: T) => void;
-    reject: (reason?: any) => void;
+    promise: (client: Client) => Promise<T>
+    resolve: (value: T) => void
+    reject: (reason?: any) => void
 }
 
 export class FTPMaster {
-    private accessOptions: AccessOptions;
-    private _maxConnections: number;
-    private _autoReconnect: boolean;
-    private queue: QueuedFTPTask[] = [];
-    private _clients: {client: Client, inUse: boolean}[] = [];
+    private accessOptions: AccessOptions
+    private _maxConnections: number
+    private _autoReconnect: boolean
+    private queue: QueuedFTPTask[] = []
+    private _clients: {client: Client, inUse: boolean}[] = []
 
-    constructor(accessOptions: AccessOptions, maxConnections: number = 1, autoReconnect: boolean = true) {
-        this.accessOptions = accessOptions;
-        this._autoReconnect = autoReconnect;
-        this._maxConnections = maxConnections;
-        this.maxConnections = maxConnections;
+    constructor(accessOptions: AccessOptions, maxConnections = 1, autoReconnect = true) {
+        this.accessOptions = accessOptions
+        this._autoReconnect = autoReconnect
+        this._maxConnections = maxConnections
+        this.maxConnections = maxConnections
     }
 
     /**
      * clear queue by rejecting all promises
      */
     public clearQueue() {
-        for(var i = 0; i < this.queue.length; i++) {
-            this.queue[i].reject("Queue cleared");
+        for(let i = 0; i < this.queue.length; i++) {
+            this.queue[i].reject("Queue cleared")
         }
-        this.queue = [];
+        this.queue = []
     }
 
     public get clients(): {client: Client, inUse: boolean}[] {
-        return this._clients;
+        return this._clients
     }
 
     private set clients(clients: {client: Client, inUse: boolean}[]) {
-        this._clients = clients;
+        this._clients = clients
     }
 
     public get autoReconnect(): boolean {
-        return this._autoReconnect;
+        return this._autoReconnect
     }
 
     public set autoReconnect(autoReconnect: boolean) {
-        this._autoReconnect = autoReconnect;
+        this._autoReconnect = autoReconnect
         if(autoReconnect) {
-            this.connectClients();
+            this.connectClients()
         }
     }
 
     public get maxConnections(): number {
-        return this._maxConnections;
+        return this._maxConnections
     }
 
     public set maxConnections(maxConnections: number) {
-        this._maxConnections = maxConnections;
+        this._maxConnections = maxConnections
 
-        for(var i = 0; i < this.maxConnections; i++) {
-            var client = new Client();
+        for(let i = 0; i < this.maxConnections; i++) {
+            const client = new Client()
             this.clients.push({
                 client,
                 inUse: false,
-            });
+            })
         }
 
         // Sort clients by priority
         this.clients.sort((a, b) => {
-            if(a.client.closed) return -2;
-            if(!a.inUse) return -1;
-            if(!b.inUse) return 1;
-            if(b.client.closed) return 2;
-            return 0;
-        });
+            if(a.client.closed) return -2
+            if(!a.inUse) return -1
+            if(!b.inUse) return 1
+            if(b.client.closed) return 2
+            return 0
+        })
 
-        for(var i = this.maxConnections; i < this.clients.length; i++) {
-            this.clients[i].client.close();
+        for(let i = this.maxConnections; i < this.clients.length; i++) {
+            this.clients[i].client.close()
         }
-        this.clients = this.clients.slice(0, this.maxConnections);
-        this.clients.reverse();
+        this.clients = this.clients.slice(0, this.maxConnections)
+        this.clients.reverse()
 
         if(this.autoReconnect) {
-            this.connectClients();
+            this.connectClients()
         }
     }
 
     public async connectClients() {
-        for(var i = 0; i < this.maxConnections; i++) {
-            await this.connectClient(this.clients[i].client);
+        for(let i = 0; i < this.maxConnections; i++) {
+            await this.connectClient(this.clients[i].client)
         }
     }
 
     private async connectClient(client: Client) {
         if(client.closed) {
-            await client.access(this.accessOptions);
+            await client.access(this.accessOptions)
             client.ftp.socket.on("close", () => {
                 if(this.autoReconnect) {
                     this.connectClient(client)
@@ -106,43 +106,43 @@ export class FTPMaster {
         }
     }
 
-    public enqueue<T = void>(promise: (client: Client) => Promise<T>, priority: boolean = false): Promise<T> {
+    public enqueue<T = void>(promise: (client: Client) => Promise<T>, priority = false): Promise<T> {
         return new Promise((resolve, reject) => {
             if(priority) {
                 this.queue.unshift({
                     promise,
                     resolve,
                     reject,
-                });
+                })
             } else {
                 this.queue.push({
                     promise,
                     resolve,
                     reject,
-                });
+                })
             }
-            this.try_dequeue();
-        });
+            this.try_dequeue()
+        })
     }
 
     private async try_dequeue(): Promise<boolean> {
-        var client = this.clients.find(x => !x.inUse && !x.client.closed);
-        if(!client) return false;
+        const client = this.clients.find(x => !x.inUse && !x.client.closed)
+        if(!client) return false
         
-        const item = this.queue.shift();
-        if (!item) return false;
+        const item = this.queue.shift()
+        if (!item) return false
 
-        client.inUse = true;
+        client.inUse = true
         item.promise(client.client).then((value) => {
-            item.resolve(value);
+            item.resolve(value)
         }).catch((err) => {
-            item.reject(err);
+            item.reject(err)
         }).finally(() => {
-            client!.inUse = false;
+            if(client) client.inUse = false
             this.try_dequeue()
-        });
+        })
         
-        return true;
+        return true
     }
 
 }
